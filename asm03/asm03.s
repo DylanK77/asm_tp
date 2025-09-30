e2399209fce643cfe1d2c5b1a56be744f5acb369
DylanK77 <dylan.klein77@gmail.com>
2025-09-30 23:33:37 +0200

    update 03 &04

--- asm03/asm03.s
+++ asm03/asm03.s
@@ -1,1 +1,27 @@
-﻿;; fichier asm03.s
+section .text
+global _start
+
+_start:
+    cmp rdi, 2
+    jl  _fail
+
+    mov rbx, [rsi+8]
+    mov al, [rbx]
+    cmp al, '4'
+    jne _fail
+    mov al, [rbx+1]
+    cmp al, '2'
+    jne _fail
+    mov al, [rbx+2]
+    cmp al, 0
+    jne _fail
+
+_success:
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+
+_fail:
+    mov rax, 60
+    mov rdi, 1
+    syscall
--- asm04/asm04.s
+++ asm04/asm04.s
@@ -1,1 +1,47 @@
-﻿;; fichier asm04.s
+section .bss
+    buf resb 32
+
+section .text
+global _start
+
+_start:
+    ; read(0, buf, 32)
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, buf
+    mov rdx, 32
+    syscall
+
+    test rax, rax
+    jz  _odd                ; rien lu 
+
+    ; trouver le dernier chiffre avant '\n' 
+    lea rsi, [buf + rax - 1]
+.find_digit:
+    cmp rsi, buf
+    jb  _odd                ; pas de chiffre trouvé 
+    mov al, [rsi]
+    cmp al, '0'
+    jb  .prev
+    cmp al, '9'
+    ja  .prev
+
+
+    sub al, '0'
+    and al, 1
+    jz  _even
+    jmp _odd
+
+.prev:
+    dec rsi
+    jmp .find_digit
+
+_even:
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+
+_odd:
+    mov rax, 60
+    mov rdi, 1
+    syscall
