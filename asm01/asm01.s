d109a03e179fca12660dea4cfd511afedccbe6f8
DylanK77 <dylan.klein77@gmail.com>
2025-09-30 12:08:30 +0200

    asm01

--- asm01/asm01.s
+++ asm01/asm01.s
@@ -1,1 +1,17 @@
-﻿;; fichier asm01.s
+section .data
+    msg db "1337", 10
+    len equ $ - msg
+
+section .text
+global _start
+
+_start:
+    mov rax, 1        ; sys_write
+    mov rdi, 1        ; stdout
+    mov rsi, msg      ; addr
+    mov rdx, len      ; len
+    syscall
+
+    mov rax, 60       ; sys_exit
+    mov rdi, 0
+    syscall
