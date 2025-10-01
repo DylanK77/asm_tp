9ae3276a6a89c7f17efa3dc5bcdfde0bc9543124
DylanK77 <dylan.klein77@gmail.com>
2025-10-01 11:24:33 +0200

    asm07 fixed

--- asm07/asm07.s
+++ asm07/asm07.s
@@ -4,64 +4,78 @@ section .bss
 section .text
 global _start
 
-atoi:
-    xor     rax, rax
-.parse:
-    mov     bl, [rsi]
-    cmp     bl, 0
-    je      .done
-    cmp     bl, '0'
-    jb      .done
-    cmp     bl, '9'
-    ja      .done
-    imul    rax, rax, 10
-    movzx   rdx, bl
-    sub     rdx, '0'
-    add     rax, rdx
-    inc     rsi
-    jmp     .parse
-.done:
-    ret
-
 _start:
-    mov     rax, 0
-    mov     rdi, 0
-    mov     rsi, buf
-    mov     rdx, 32
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, buf
+    mov rdx, 32
     syscall
 
-    mov     rsi, buf
-    call    atoi
-    mov     rbx, rax
+    mov rsi, buf
+    xor rax, rax
+    xor r8d, r8d        ; saw_digit
+    xor r9d, r9d        ; bad
 
-    cmp     rbx, 2
-    jl      _notprime
+.parse:
+    mov bl, [rsi]
+    cmp bl, 10
+    je  .after
+    cmp bl, 0
+    je  .after
+    cmp bl, '0'
+    jb  .badch
+    cmp bl, '9'
+    ja  .badch
+    imul rax, rax, 10
+    movzx rdx, bl
+    sub rdx, '0'
+    add rax, rdx
+    inc rsi
+    mov r8d, 1
+    jmp .parse
 
-    mov     rcx, 2
+.badch:
+    mov r9d, 1
+.after:
+    test r8d, r8d
+    jz  _invalid
+    test r9d, r9d
+    jnz _invalid
+
+    mov rbx, rax
+    cmp rbx, 2
+    jl  _notprime
+
+    mov rcx, 2
 .loop:
-    mov     rax, rbx
-    xor     rdx, rdx
-    div     rcx
-    test    rdx, rdx
-    jz      .check_equal
-    inc     rcx
-    mov     rax, rcx
-    imul    rax, rax
-    cmp     rax, rbx
-    jbe     .loop
-    jmp     _prime
+    mov rax, rbx
+    xor rdx, rdx
+    div rcx
+    test rdx, rdx
+    jz  .checkeq
+    inc rcx
+    mov rax, rcx
+    imul rax, rax
+    cmp rax, rbx
+    jbe .loop
+    jmp _prime
 
-.check_equal:
-    cmp     rcx, rbx
-    je      _prime
-    jmp     _notprime
+.checkeq:
+    cmp rcx, rbx
+    je  _prime
+    jmp _notprime
 
 _prime:
-    mov     rax, 60
-    xor     rdi, rdi
+    mov rax, 60
+    xor rdi, rdi
     syscall
 
 _notprime:
-    mov     rax, 60
-    mov     rdi, 1
+    mov rax, 60
+    mov rdi, 1
+    syscall
+
+_invalid:
+    mov rax, 60
+    mov rdi, 2
     syscall
