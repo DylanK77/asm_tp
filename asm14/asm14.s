dd02c53ca640b7974b07877e6cc7413050afe495
DylanK77 <dylan.klein77@gmail.com>
2025-10-03 10:38:22 +0200

    asm14 fixed

--- asm14/asm14.s
+++ asm14/asm14.s
@@ -6,32 +6,34 @@ section .text
 global _start
 
 _start:
-    mov     rax, [rsp]          ; argc
+    mov     rax, [rsp]
     cmp     rax, 2
     jl      exit1
 
-    mov     rsi, [rsp+16]       ; argv[1] = filename
-    mov     rax, 2              ; sys_open
-    mov     rdi, rsi
-    mov     rsi, 577            ; O_CREAT|O_WRONLY|O_TRUNC = 0x241
-    mov     rdx, 0644
+    mov     rsi, [rsp+16]       ; filename*
+
+    ; openat(AT_FDCWD, filename, O_CREAT|O_WRONLY|O_TRUNC, 0644)
+    mov     rax, 257            ; sys_openat
+    mov     rdi, -100           ; AT_FDCWD
+    ; rsi already filename
+    mov     rdx, 577            ; O_CREAT|O_WRONLY|O_TRUNC
+    mov     r10, 420            ; 0644 in decimal
     syscall                     ; rax = fd
 
     mov     rdi, rax
-    mov     rax, 1              ; sys_write
+    mov     rax, 1              ; write(fd, msg, len)
     mov     rsi, msg
     mov     rdx, len
     syscall
 
-    mov     rax, 3              ; sys_close
+    mov     rax, 3              ; close(fd)
     syscall
 
-exit0:
-    mov     rax, 60
+    mov     rax, 60             ; exit(0)
     xor     rdi, rdi
     syscall
 
 exit1:
-    mov     rax, 60
+    mov     rax, 60             ; exit(1)
     mov     rdi, 1
     syscall
